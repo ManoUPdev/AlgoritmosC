39f64db48d3e4e22dfdee0a40fc28b748a2f4a7b
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-07-02 21:15:07 -0300

    victor 5

--- Q1/Hotel.c
+++ Q1/Hotel.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 float calcular_total_conta(float valor_diaria, int num_dias, float valor_frigobar) {
     float subtotal = valor_diaria * num_dias + valor_frigobar;
     float imposto_municipal = subtotal * 0.035;
@@ -7,23 +8,25 @@ float calcular_total_conta(float valor_diaria, int num_dias, float valor_frigoba
     return total_conta;
 }
 
-int main (){
+int main() {
     float valor_diaria, valor_frigobar;
     int num_dias;
 
-    printf("Informe o valor da diaria do hotel(R$): ");
-    scanf("%f",&valor_diaria);
+
+    printf("Informe o valor da diaria do hotel (R$): ");
+    scanf("%f", &valor_diaria);
 
     printf("Informe o numero de dias: ");
     scanf("%d", &num_dias);
 
-    printf("Informe o valor das despesas de frigobar(R$): ");
+    printf("Informe o valor das despesas de frigobar (R$): ");
     scanf("%f", &valor_frigobar);
-    
 
+    
     float total_conta = calcular_total_conta(valor_diaria, num_dias, valor_frigobar);
 
-    printf("O valor total a pagar e: R$ %.2f/n", total_conta)
+    
+    printf("O valor total a pagar e: R$ %.2f\n", total_conta);
 
     return 0;
 }
