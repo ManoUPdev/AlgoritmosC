723ba05d29fca11077214598cf4f5d65de70bca0
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-07-02 21:29:06 -0300

    victor 6

---
+++ Q3/Pizzas.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+    float preco_queijo_kg;
+    char tamanho_pizza;
+    int num_fatias;
+    float peso_queijo_por_fatia = 15.0 / 1000.0;
+    float custo_queijo;
+
+
+    printf("Informe o preco por Kg de queijo utilizado (R$/kg): ");
+    scanf("%f", &preco_queijo_kg);
+
+    printf("informe o tamanho da pizza(P, M, G, F):");
+    scanf("%c", &tamanho_pizza);
+
+    switch (tamanho_pizza) {
+        
+    }
+
+
+}
