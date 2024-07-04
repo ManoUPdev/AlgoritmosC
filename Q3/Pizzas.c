6208bcf12a1d545fdf3c2f9b75ab9fc7e5e23214
ManoUPdev <emanuelcgalindo@hotmail.com>
2024-07-03 22:23:17 -0300

    victor 7

--- Q3/Pizzas.c
+++ Q3/Pizzas.c
@@ -15,8 +15,32 @@ int main () {
     scanf("%c", &tamanho_pizza);
 
     switch (tamanho_pizza) {
+        case 'P':
+           num_fatias = 6;
+           break;
+
+        case 'M':
+           num_fatias = 8;
+           break;
+
+        case 'G':
+            num_fatias = 10;
+            break;
+
+        case 'B':
+             num_fatias = 12;
+             break;
+          default:
+               printf("Tamanho de pizza invalido.\n");
+               return 1;
         
     }
+
+    custo_queijo = num_fatias * peso_queijo_por_fatia * preco_queijo_kg;
+
+    printf("O custo do queijo da pizza e de: R$%.2f\n", custo_queijo);
+
+    return 0;
 
 
 }
